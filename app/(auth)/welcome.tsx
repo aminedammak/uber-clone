81add6810469f4959eaafc9d789ce6d70b3b23a4
Amine Dammak <amine.dammak@foodomarket.com>
2024-12-14 16:50:48 +0100

    Add types for images

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,7 +1,11 @@
+import { onBoarding } from '@/constants';
 import { router } from 'expo-router';
+import { useState } from 'react';
 import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import Swiper from 'react-native-swiper';
 
 const OnBoarding = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
@@ -10,6 +14,21 @@ const OnBoarding = () => {
       >
         <Text className="text-black text-md font-JakartaBold">Skip</Text>
       </TouchableOpacity>
+      <Swiper
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full " />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onBoarding.map((item) => (
+          <View key={item.id}>
+            <Text>{item.title}</Text>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 };
