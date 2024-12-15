1c1d791f6442060fbc836da2a52398ee40ba2b54
amine.dammak <amine.dammak@foodomarket.com>
2024-12-15 15:13:14 +0100

    Fix nativewind preset

--- Components/CustomButton.tsx
+++ Components/CustomButton.tsx
@@ -1,43 +1,43 @@
-import { ButtonProps } from '@/types/type';
-import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { ButtonProps } from "@/types/type";
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
 
-const getBgVariantStyle = (variant: ButtonProps['bgVariant']) => {
+const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
   switch (variant) {
-    case 'secondary':
-      return 'bg-gray-500';
-    case 'danger':
-      return 'bg-red-500';
-    case 'success':
-      return 'bg-green-500';
-    case 'outline':
-      return 'bg-transparent border-neutral-300 border-[0.5px]';
+    case "secondary":
+      return "bg-gray-500";
+    case "danger":
+      return "bg-red-500";
+    case "success":
+      return "bg-green-500";
+    case "outline":
+      return "bg-transparent border-neutral-300 border-[0.5px]";
     default:
-      return 'bg-[#0286FF]';
+      return "bg-[#0286FF]";
   }
 };
 
-const getTextVariantStyle = (variant: ButtonProps['textVariant']) => {
+const getTextVariantStyle = (variant: ButtonProps["textVariant"]) => {
   switch (variant) {
-    case 'primary':
-      return 'text-black';
-    case 'secondary':
-      return 'text-gray-100';
-    case 'danger':
-      return 'text-red-100';
-    case 'success':
-      return 'text-green-100';
+    case "primary":
+      return "text-black";
+    case "secondary":
+      return "text-gray-100";
+    case "danger":
+      return "text-red-100";
+    case "success":
+      return "text-green-100";
 
     default:
-      return 'text-white';
+      return "text-white";
   }
 };
 
 export default function CustomButton({
   onPress,
   title,
-  bgVariant = 'primary',
-  textVariant = 'default',
+  bgVariant = "primary",
+  textVariant = "default",
   IconLeft,
   IconRight,
   className,
@@ -46,7 +46,7 @@ export default function CustomButton({
   return (
     <TouchableOpacity
       onPress={onPress}
-      className={`w-full rounded-full flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}`}
+      className={`w-full rounded-full flex flex-row p-3 justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}`}
     >
       {IconLeft && <IconLeft />}
       <Text className={`text-lg font-bold ${getTextVariantStyle(textVariant)}`}>
